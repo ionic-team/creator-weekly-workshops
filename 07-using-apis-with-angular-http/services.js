a80fa91041932ac522a90dd1c1ffcfee086499a5
Matthew Kremer <mkremer90@gmail.com>
2016-10-20 11:35:29 -0500

    Update to use Promise passthrough

--- 07-using-apis-with-angular-http/services.js
+++ 07-using-apis-with-angular-http/services.js
@@ -1,32 +1,27 @@
 angular.module('app.services', [])
 
-.service('Survey', ['$q', '$http', function($q, $http){
-    
+.service('Survey', ['$http', function($http){
+
     var api_url = 'YOUR_SHEETSU_URL';
     var currentID = 1;
-    
+
     var ret = {
         all: function(){
-            var deferred = $q.defer();
             
-            $http.get(api_url).then(function(resp){
+            return $http.get(api_url).then(function(resp){
                 if (resp.data.length > 0) currentID = parseInt(resp.data[resp.data.length-1].id);
-                deferred.resolve(resp.data);
+                return resp.data;
             });
             
-            return deferred.promise;
-        },
+        }, 
         add: function(data){
-            var deferred = $q.defer();
-            
             currentID++;
             data.id = currentID;
             
-            $http.post(api_url+'/column/val', data).then(function(resp){
-                deferred.resolve(resp.data); 
+            return $http.post(api_url, data).then(function(resp){
+                return resp.data;
             });
-            
-            return deferred.promise;
+
         }
     }
     
